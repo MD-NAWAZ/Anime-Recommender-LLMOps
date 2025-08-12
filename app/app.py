98e0972374cc0b209fba78139178e52e5b15bb8c
MD-NAWAZ <sunny9007059957@gmailcom>
2025-08-13 01:03:15 +0530

    commit

---
+++ app/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from pipeline.pipeline import AnimeRecommendationPipeline
+from dotenv import load_dotenv
+
+
+
+st.set_page_config(page_title="Anime Recommender", layout="wide")
+
+load_dotenv()
+
+@st.cache_resource
+def init_pipeline():
+    return AnimeRecommendationPipeline()
+
+pipeline = init_pipeline()
+
+st.title("Anime Recommender System")
+
+query = st.text_input("Enter your Anime Preferences eg: light hearted anime with school settings")
+if query:
+    with st.spinner("fetching recommendation for you"):
+        response = pipeline.recommend(query)
+        st.markdown("### Recommendation")
+        st.write(response)
+
+
